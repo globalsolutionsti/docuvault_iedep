f87de88d5284ef5af365f4b62a61bbc8227ab83f
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-04-20 11:55:22 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,23 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyUy1NhZSX1BWKFA3mDRcs8o3FUbfYyYFzkuq8VpnbPqhYRykpqF2G1mLYPeM2l5O6t/exec";
 
+function login() {
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
 
-function login(usuario, password) {
-  const ss = SpreadsheetApp.openById(SHEET_ID);
-  const sheet = ss.getSheetByName(HOJA_USUARIOS);
+  const url = `${API_URL}?action=login&usuario=${usuario}&password=${password}`;
 
-  if (!sheet) {
-    return { status: "error", message: "Hoja no encontrada" };
-  }
-
-  const rows = sheet.getDataRange().getValues();
-
-  for (let i = 1; i < rows.length; i++) {
-
-    let userSheet = String(rows[i][1]).trim();
-    let passSheet = String(rows[i][2]).trim();
-    let estado = String(rows[i][5]).trim();
-
-    if (
-      userSheet === String(usuario).trim() &&
-      passSheet === String(password).trim() &&
-      estado.toLowerCase() === "activo"
-    ) {
-      return {
-        status: "success",
-        user: {
-          id: rows[i][0],
-          nombre: rows[i][3],
-          rol: rows[i][4]
-        }
-      };
-    }
-  }
-
-  return {
-    status: "error",
-    message: "Usuario o contraseña incorrectos"
-  };
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === "success") {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        window.location.href = "dashboard.html";
+      } else {
+        document.getElementById("error").innerText = data.message;
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById("error").innerText = "Error de conexión";
+    });
 }
