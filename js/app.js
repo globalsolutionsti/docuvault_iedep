6dde5bf33b642436b5b6e0062318f409f7481b61
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-04-20 11:45:26 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,26 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyyHb26M_W7igJ1Isk2hjJG53ePpxQPNrc0vezA3l3eEcCUF6FWjg2xg7YM369b3T_D/exec";
 
+
 function login() {
   const usuario = document.getElementById("usuario").value;
   const password = document.getElementById("password").value;
 
-  fetch(API_URL, {
-    method: "POST",
-    body: JSON.stringify({
-      action: "login",
-      usuario: usuario,
-      password: password
+  const url = `${API_URL}?action=login&usuario=${usuario}&password=${password}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === "success") {
+
+        localStorage.setItem("user", JSON.stringify(data.user));
+        window.location.href = "dashboard.html";
+
+      } else {
+        document.getElementById("error").innerText = data.message;
+      }
     })
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (data.status === "success") {
-      
-      // Guardar sesión
-      localStorage.setItem("user", JSON.stringify(data.user));
-
-      // Redirigir (siguiente fase)
-      window.location.href = "dashboard.html";
-
-    } else {
-      document.getElementById("error").innerText = data.message;
-    }
-  });
+    .catch(err => {
+      console.error(err);
+      document.getElementById("error").innerText = "Error de conexión";
+    });
 }
