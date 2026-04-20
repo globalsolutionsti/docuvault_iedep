92666fa6f2132de04db7abd2b7202ded4e8460db
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-04-20 11:51:01 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyyHb26M_W7igJ1Isk2hjJG53ePpxQPNrc0vezA3l3eEcCUF6FWjg2xg7YM369b3T_D/exec";
 
 
-function login() {
-  const usuario = document.getElementById("usuario").value;
-  const password = document.getElementById("password").value;
+function login(usuario, password) {
+  const ss = SpreadsheetApp.openById(SHEET_ID);
+  const sheet = ss.getSheetByName(HOJA_USUARIOS);
 
-  const url = `${API_URL}?action=login&usuario=${usuario}&password=${password}`;
+  if (!sheet) {
+    return { status: "error", message: "Hoja no encontrada" };
+  }
 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      if (data.status === "success") {
+  const rows = sheet.getDataRange().getValues();
 
-        localStorage.setItem("user", JSON.stringify(data.user));
-        window.location.href = "dashboard.html";
+  for (let i = 1; i < rows.length; i++) {
 
-      } else {
-        document.getElementById("error").innerText = data.message;
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      document.getElementById("error").innerText = "Error de conexión";
-    });
+    let userSheet = String(rows[i][1]).trim();
+    let passSheet = String(rows[i][2]).trim();
+    let estado = String(rows[i][5]).trim();
+
+    if (
+      userSheet === String(usuario).trim() &&
+      passSheet === String(password).trim() &&
+      estado.toLowerCase() === "activo"
+    ) {
+      return {
+        status: "success",
+        user: {
+          id: rows[i][0],
+          nombre: rows[i][3],
+          rol: rows[i][4]
+        }
+      };
+    }
+  }
+
+  return {
+    status: "error",
+    message: "Usuario o contraseña incorrectos"
+  };
 }
